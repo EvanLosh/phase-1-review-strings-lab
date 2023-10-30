705e96e5e25f024df6d22045a0c31b0c1e3501c8
Evan Losh <evand.losh@gmail.com>
2023-10-30 15:24:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Bugs Bunny";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` + currentUser.toUpperCase() + `!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ` + currentUser.toUpperCase() + `!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
